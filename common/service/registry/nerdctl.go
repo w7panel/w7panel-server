ea26dff7cdc51ba1d1ca331b5996411f1fde66cf
fykyx521 <446897682@qq.com>
2026-03-30 07:46:30 +0000

    add: nerdctl

---
+++ common/service/registry/nerdctl.go
@@ -0,0 +1,20 @@
+package registry
+
+import (
+	"context"
+
+	// "github.com/containerd/containerd"
+	containerd "github.com/containerd/containerd/v2/client"
+	"github.com/containerd/nerdctl/v2/pkg/api/types"
+	"github.com/containerd/nerdctl/v2/pkg/cmd/container"
+	"github.com/containerd/nerdctl/v2/pkg/cmd/image"
+)
+
+// commit a image
+func Commit(ctx context.Context, client *containerd.Client, rawRef string, req string, options types.ContainerCommitOptions) error {
+	return container.Commit(ctx, client, rawRef, req, options)
+}
+
+func Pull(ctx context.Context, client *containerd.Client, rawRef string, req string, options types.ImagePullOptions) error {
+	return image.Pull(ctx, client, rawRef, options)
+}
