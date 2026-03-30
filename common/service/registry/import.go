3746ba82b57e5f267c9eb2bcfff9ca8ce339e5a0
fykyx521 <446897682@qq.com>
2026-03-30 06:46:22 +0000

    fix: nerdctl

--- common/service/registry/import.go
+++
@@ -1,8 +0,0 @@
-package registry
-
-// "github.com/containerd/nerdctl/v2/pkg/cmd/image"
-
-// func Import(ctx context.Context, client *containerd.Client, options types.ImageImportOptions) (string, error) {
-// 	// cmd.Import()
-// 	// image.Import()
-// }
