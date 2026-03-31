c5c0bf7565381b5cbcae2f65176276b8512ba7e8
fykyx521 <446897682@qq.com>
2026-03-31 11:08:34 +0000

    build:test

--- common/service/k8s/zpk/manifestconvert.go
+++ common/service/k8s/zpk/manifestconvert.go
@@ -22,7 +22,8 @@ import (
 	// appsv1 "k8s.io/client-go/applyconfigurations/apps/v1"
 )
 
-const buildimage = "ccr.ccs.tencentyun.com/afan-public/kaniko:w7console-new5-19"
+// const buildimage = "ccr.ccs.tencentyun.com/afan-public/kaniko:w7console-new5-19"
+const buildimage = "ccr.ccs.tencentyun.com/afan-public/kaniko:w7console-build-test1"
 
 type K8sResourceMetaInterface interface {
 	GetTitle() string
