7306ab093ab664ee06c83008b705a5dffa70ddfe
fykyx521 <446897682@qq.com>
2026-04-01 02:30:53 +0000

    fix: buildimage

--- common/service/k8s/zpk/manifestconvert.go
+++ common/service/k8s/zpk/manifestconvert.go
@@ -22,7 +22,7 @@ import (
 	// appsv1 "k8s.io/client-go/applyconfigurations/apps/v1"
 )
 
-var buildimage = "ccr.ccs.tencentyun.com/afan-public/kaniko:w7console-new5-19"
+var buildimage = "ccr.ccs.tencentyun.com/afan-public/kaniko:w7console-new5-20"
 
 // convvst buildimage = "ccr.ccs.tencentyun.com/afan-public/kaniko:w7console-build-test1"
 func init() {
