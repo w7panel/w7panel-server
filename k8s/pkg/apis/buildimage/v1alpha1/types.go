d01583312043e651a6b3495b305da030f7b367ca
cnb.ZUpyyEiJQGA <qAfFqfmKkran8a3IGs8hOL+cnb.ZUpyyEiJQGA@noreply.cnb.cool>
2026-04-07 22:20:12 +0800

    feat: k8s types

---
+++ k8s/pkg/apis/buildimage/v1alpha1/types.go
@@ -0,0 +1,39 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type BuildImage struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec              BuildImageSpec `json:"spec"`
+}
+
+type BuildImageSpec struct {
+	TaskID    string `json:"taskId"`
+	Namespace string `json:"namespace"`
+	Source    struct {
+		DownloadURL    string `json:"downloadUrl"`
+		DockerfilePath string `json:"dockerfilePath"`
+	} `json:"source"`
+	TargetImage struct {
+		Address string `json:"address"`
+		Auth    struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		} `json:"auth"`
+	} `json:"targetImage"`
+	NotifyURL string `json:"notifyUrl"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+type BuildImageList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+
+	Items []BuildImage `json:"items"`
+}
