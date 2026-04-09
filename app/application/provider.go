bf73a2f6ae4f6894230906381cd608932092c11e
rxwyun <1120309488@qq.com>
2026-04-09 13:45:18 +0800

    add: container image export

--- app/application/provider.go
+++ app/application/provider.go
@@ -241,6 +241,11 @@ func (p Provider) RegisterHttpRoutes(server *httpserver.Server) {
 		engine.GET("/panel-api/v1/microapp/top", middleware.Auth{}.Process, controller2.MicroApp{}.List)                     //获取microapp列表
 		engine.Any("/panel-api/v1/microapp/:name/proxy/*path", middleware.Auth{}.Process, controller2.Proxy{}.ProxyMicroApp) //microapp proxy
 
+		containerGroup := localApiGroup.Group("/containers", middleware.Auth{}.Process)
+		{
+			containerGroup.POST("/image/export-push", controller2.Container{}.ExportAndPushImage)
+		}
+
 	})
 }
 
