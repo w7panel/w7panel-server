8abc6c6f4af22c48bbd9ee530887206fb5ef4f9d
rxwyun <1120309488@qq.com>
2026-04-09 14:28:41 +0800

    fix: container image export use contained/v2

--- common/service/k8s/container/container.go
+++ common/service/k8s/container/container.go
@@ -10,9 +10,9 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/containerd/containerd"
-	"github.com/containerd/containerd/mount"
-	"github.com/containerd/containerd/namespaces"
+	containerd "github.com/containerd/containerd/v2/client"
+	"github.com/containerd/containerd/v2/core/mount"
+	"github.com/containerd/containerd/v2/pkg/namespaces"
 	"github.com/w7panel/w7panel/common/helper"
 )
 
