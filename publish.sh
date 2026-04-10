02db0e7186f3deb95b18236c010c50ba4581ed89
fykyx521 <446897682@qq.com>
2026-04-10 06:39:45 +0000

    ui: fix

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 helm upgrade w7panel-offline ./kodata/charts/k8s-offline \
 	 --set mock.upgrade=true --set servicelb.loadBalancerClass=io.cilium/node --set webhook.enabled=true \
-	  --set servicelb.port=9090 --set image.tag=v1.1.40.7 --set image.repository=docker.cnb.cool/i0358/zpk --set controller.appWatch=true --set k3k.watch=true  --set mock.upgrade=true \
+	  --set servicelb.port=9090 --set image.tag=v1.1.41.1 --set image.repository=docker.cnb.cool/i0358/zpk --set controller.appWatch=true --set k3k.watch=true  --set mock.upgrade=true \
 	--set storage.enabled=false --install
 
 
