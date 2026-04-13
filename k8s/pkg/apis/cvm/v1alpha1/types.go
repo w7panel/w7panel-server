03a2fbe4298b2f184046fdfca4e02e2fe29e6172
fykyx521 <446897682@qq.com>
2026-04-13 08:20:03 +0000

    feat: add cvm config

--- k8s/pkg/apis/cvm/v1alpha1/types.go
+++ k8s/pkg/apis/cvm/v1alpha1/types.go
@@ -15,17 +15,26 @@ type Cvm struct {
 }
 
 type CvmSpec struct {
-	CPU       int64 `json:"cpu,omitempty"`
-	Memory    int64 `json:"memory,omitempty"`
-	Storage   int64 `json:"storage,omitempty"`
-	Bandwidth int64 `json:"bandwidth,omitempty"`
+	CPU              int64    `json:"cpu,omitempty"`
+	Memory           int64    `json:"memory,omitempty"`
+	Storage          int64    `json:"storage,omitempty"`
+	Bandwidth        int64    `json:"bandwidth,omitempty"`
+	StorageSize      int64    `json:"storageSize,omitempty"`
+	StorageClassName string   `json:"storageClassName,omitempty"`
+	Workload         Workload `json:"workload,omitempty"`
 }
 
+type Workload struct {
+	metav1.TypeMeta `json:",inline"`
+	PodTemplateName string `json:"podTemplateName"`
+}
+
+// 【微擎面板&集群云主机：云主机业务分离成独立应用】
+// https://www.tapd.cn/tapd_fe/62789787/story/detail/1162789787001015242
 type CvmStatus struct {
-	CPU       int64 `json:"cpu,omitempty"`
-	Memory    int64 `json:"memory,omitempty"`
-	Storage   int64 `json:"storage,omitempty"`
-	Bandwidth int64 `json:"bandwidth,omitempty"`
+	Phase         string             `json:"phase,omitempty"`
+	ReadyReplicas int32              `json:"readyReplicas,omitempty"`
+	Conditions    []metav1.Condition `json:"conditions,omitempty"`
 }
 
 // +genclient
