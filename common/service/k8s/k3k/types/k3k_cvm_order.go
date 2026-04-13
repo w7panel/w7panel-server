a49fba4ef59b7e40392fbb517c58fa39969630d8
fykyx521 <446897682@qq.com>
2026-04-13 10:25:21 +0000

    feat: cvm order type

---
+++ common/service/k8s/k3k/types/k3k_cvm_order.go
@@ -0,0 +1,204 @@
+package types
+
+import (
+	"errors"
+	"time"
+
+	"github.com/w7panel/w7panel/common/service/console"
+	"github.com/w7panel/w7panel/common/service/k8s/k3k/overselling"
+	corev1 "k8s.io/api/core/v1"
+)
+
+type K3kCvmOrder struct {
+	*corev1.ServiceAccount
+	*k3kUserOverSelling
+	*k3kUserTime
+	cost *K3kCost
+}
+
+func Newk3kCvmOrder(sa *corev1.ServiceAccount, overUser *k3kUserOverSelling, userTime *k3kUserTime, cost *K3kCost) *K3kCvmOrder {
+	return &K3kCvmOrder{sa, overUser, userTime, cost}
+}
+
+func (u *K3kCvmOrder) SetBaseOrder(orderSn string) {
+	u.Labels[W7_BASE_ORDER_SN] = orderSn
+}
+
+func (u *K3kCvmOrder) SetRenewOrder(orderSn string) {
+	u.Labels[W7_RENEW_ORDER_SN] = orderSn
+	u.Labels[W7_RENEW_ORDER_STATUS] = W7_ORDER_WAIT //因为支付成功后会判断是否PAID，所以这里先设置为等待支付
+}
+
+func (u *K3kCvmOrder) SetExpandOrder(orderSn string) {
+	u.Labels[W7_EXPAND_ORDER_SN] = orderSn
+	u.Labels[W7_EXPAND_ORDER_STATUS] = W7_ORDER_WAIT
+}
+
+func (u *K3kCvmOrder) GetBaseOrderSn() string {
+	return u.Labels[W7_BASE_ORDER_SN]
+}
+
+func (u *K3kCvmOrder) GetRenewOrderSn() string {
+	return u.Labels[W7_RENEW_ORDER_SN]
+}
+
+func (u *K3kCvmOrder) GetExpandOrderSn() string {
+	return u.Labels[W7_EXPAND_ORDER_SN]
+}
+
+func (u *K3kCvmOrder) SetBaseOrderPaid(info *console.OrderInfo) {
+	if u.Labels[W7_BASE_ORDER_SN] == info.OrderSn && u.Labels[W7_BASE_ORDER_STATUS] != W7_ORDER_PAID {
+		// u.Labels[K3K_BUY_MODE] = "buy"
+		u.Labels[W7_BASE_ORDER_STATUS] = W7_ORDER_PAID
+		u.changeExpireTime(int(info.GetHour()))
+		rs := overselling.OrderInfoToResource(info)
+		u.Annotations[W7_OVER_BASE_RESOURCE] = rs.JsonString()
+		u.Annotations[W7_QUOTA_LIMIT_LOCK] = "true" //锁定配额，防止配额被费用套餐覆盖
+		u.Labels[W7_OVER_MODE] = "wait"
+
+	}
+}
+
+func (u *K3kCvmOrder) SetRenewOrderPaid(info *console.OrderInfo) {
+	if u.Labels[W7_RENEW_ORDER_SN] == info.OrderSn && u.Labels[W7_RENEW_ORDER_STATUS] != W7_ORDER_PAID {
+		// u.Labels[K3K_BUY_MODE] = "renew"
+		u.Labels[W7_RENEW_ORDER_STATUS] = W7_ORDER_PAID
+		u.changeExpireTime(int(info.GetHour()))
+	}
+}
+
+func (u *K3kCvmOrder) SetExpandOrderPaid(info *console.OrderInfo) {
+	if u.Labels[W7_EXPAND_ORDER_SN] == info.OrderSn && u.Labels[W7_EXPAND_ORDER_STATUS] != W7_ORDER_PAID {
+		// u.Labels[K3K_BUY_MODE] = "renew"
+		u.Labels[W7_EXPAND_ORDER_STATUS] = W7_ORDER_PAID
+		// u.changeExpireTime(hour)
+		u.Annotations[W7_OVER_RESOURCE] = overselling.OrderInfoToResource(info).JsonString()
+		u.Labels[W7_OVER_MODE] = "wait"
+	}
+}
+
+func (u *K3kCvmOrder) SetOrderStatus(info *console.OrderInfo) {
+	if info.OrderStatus != "paid" {
+		return
+	}
+	switch info.BuyMode {
+	case "base":
+		u.SetBaseOrderPaid(info)
+		break
+	case "renew":
+		u.SetRenewOrderPaid(info)
+		break
+	case "expand":
+		u.SetExpandOrderPaid(info)
+		break
+	}
+}
+
+func (u *K3kCvmOrder) NeedCreateOrder() bool {
+	pass, ok := u.Labels[W7_BASE_ORDER_PASS]
+	if ok {
+		return pass == "false"
+	}
+	if u.NeedBuyResource() {
+		status, ok := u.Labels[W7_BASE_ORDER_STATUS]
+		if !ok {
+			return true
+		}
+		if status == W7_ORDER_PAID {
+			return false
+		}
+		return true
+	}
+	return false
+}
+
+// 是否可以续费
+
+// 到期后必须续费，否则无法使用
+func (u *K3kCvmOrder) NeedRenew() bool {
+	if u.NeedBuyResource() {
+		expireTime, err := u.GetExpireTime()
+		if err != nil {
+			return false
+		}
+		if expireTime.Before(time.Now()) { //3天内可以续费
+			// if expireTime.Before(time.Now().Add(-time.Hour * 72)) { //3天内可以续费
+			return true
+		}
+	}
+	// if err := u.CanRenewError(); err != nil {
+	// 	expireTime, err := u.GetExpireTime()
+	// 	if err != nil {
+	// 		return false
+	// 	}
+	// 	if expireTime.Before(time.Now()) { //3天内可以续费
+	// 		// if expireTime.Before(time.Now().Add(-time.Hour * 72)) { //3天内可以续费
+	// 		return true
+	// 	}
+	// }
+	return false
+}
+
+func (u *K3kCvmOrder) CanCreateBaseOrderError() error {
+	pass, ok := u.Labels[W7_BASE_ORDER_PASS]
+	if ok {
+		if pass == "false" {
+			return nil
+		}
+	}
+	if u.NeedBuyResource() {
+		status, ok := u.Labels[W7_BASE_ORDER_STATUS]
+		if !ok {
+			return nil
+		}
+		if status == W7_ORDER_PAID {
+			return errors.New("已经购买基础资源，无法重复购买")
+		}
+		return nil
+	}
+	return errors.New("当前用户未配置费用套餐，无法购买")
+}
+
+func (u *K3kCvmOrder) CanRenewError() error {
+	if u.NeedBuyResource() {
+		_, err := u.GetExpireTime() // 如果没有过期时间，则不需要续费
+		if err != nil {
+			return errors.New("未购买基础资源，无需购买")
+		}
+		return nil
+	}
+	return errors.New("当前用户未配置费用套餐，无法购买")
+}
+
+func (u *K3kCvmOrder) CanExpandError() error {
+	if !u.IsOverSellingSuccess() {
+		return errors.New("超额检查失败，无法扩容")
+	}
+	if u.NeedBuyResource() {
+		extime, err := u.GetExpireTime() // 如果没有过期时间，则不需要续费
+		if err != nil {
+			return errors.New("未购买基础资源，无法扩容")
+		}
+		ok := extime.After(time.Now())
+		if ok {
+			return nil
+		}
+		return errors.New("基础资源已过期，无法扩容")
+	}
+	return errors.New("当前用户未配置费用套餐，无法购买")
+}
+
+func (u *K3kCvmOrder) NeedBuyResource() bool {
+	if u.cost != nil {
+		return true
+	}
+	return false
+}
+
+func (u *K3kCvmOrder) HasProcessReturnOrder() bool {
+	data, ok := u.Annotations[W7_RETURN_ORDER_INFO]
+	if ok && data != "" {
+		return true
+	}
+	return false
+}
