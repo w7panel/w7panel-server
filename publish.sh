729ae1dd39c2ad48f0ceb3c0322541632e17462c
fykyx521 <446897682@qq.com>
2026-04-15 10:56:37 +0000

    ui: fix

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 helm upgrade w7panel-offline ./kodata/charts/k8s-offline \
 	 --set mock.upgrade=true --set servicelb.loadBalancerClass=io.cilium/node --set webhook.enabled=true \
-	  --set servicelb.port=9090 --set image.tag=v1.1.44.8 --set image.repository=docker.cnb.cool/i0358/zpk --set controller.appWatch=true --set k3k.watch=true  --set mock.upgrade=true \
+	  --set servicelb.port=9090 --set image.tag=v1.1.44.9 --set image.repository=docker.cnb.cool/i0358/zpk --set controller.appWatch=true --set k3k.watch=true  --set mock.upgrade=true \
 	--set storage.enabled=false --install
 
 # https://oneuptime.com/blog/post/2026-01-30-longhorn-volume-snapshots/view
