0142d9340df8172cd03e0fcd552e972f4d5015aa
fykyx521 <446897682@qq.com>
2026-04-20 11:33:40 +0000

    ui: fix

--- common/service/k8s/k3k/types/k8sresource.go
+++ common/service/k8s/k3k/types/k8sresource.go
@@ -638,7 +638,7 @@ func ToK3kWeihJob(k3kUser *K3kUser, jobName string) *batchv1.Job {
 			Name:        jobName,
 			Labels:      labels,
 			Annotations: annotations,
-			Namespace:   k3kUser.GetNamespace(),
+			Namespace:   k3kUser.GetK3kNamespace(),
 		},
 		Spec: batchv1.JobSpec{
 			TTLSecondsAfterFinished: &afterSeconds,
