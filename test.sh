962545ada257347c053cee60dd900d63986f6859
fykyx521 <446897682@qq.com>
2026-04-21 11:26:27 +0000

    rm: publish

--- test.sh
+++ test.sh
@@ -17,3 +17,14 @@ ctr run --rm ccr.ccs.tencentyun.com/afan-public/nginx:test nginx-test1
 ctr run --rm registry.local.w7.cc/afan-public/n:v1 nginx-test123
 
 sudo ctr task exec --exec-id shell -t nginx-test1 /bin/bash
+
+helm upgrade w7panel-offline ./kodata/charts/k8s-offline \
+	 --set mock.upgrade=true --set servicelb.loadBalancerClass=io.cilium/node --set webhook.enabled=true \
+	  --set servicelb.port=9090 --set image.tag=v1.1.48.43 --set image.repository=docker.cnb.cool/i0358/zpk --set controller.appWatch=true --set k3k.watch=true  --set mock.upgrade=true \
+	--set storage.enabled=false --install 
+
+# https://oneuptime.com/blog/post/2026-01-30-longhorn-volume-snapshots/view
+# helm --kubeconfig=/home/workspace/.kube/162.config upgrade w7panel-offline ./kodata/charts/k8s-offline \
+# 	 --set mock.upgrade=true --set servicelb.loadBalancerClass=io.cilium/node --set webhook.enabled=true \
+# 	  --set servicelb.port=9090 --set image.tag=v1.1.44.6 --set image.repository=docker.cnb.cool/i0358/zpk --set controller.appWatch=true --set k3k.watch=true  --set mock.upgrade=true \
+# 	--set storage.enabled=false --install
