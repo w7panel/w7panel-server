fd3a32dad03ace4d4bc54ada36eb33a64c853849
fykyx521 <446897682@qq.com>
2026-05-27 08:43:53 +0000

    clean: kodata

--- kodata/shell/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kubectl get jobs -n default -o json \
-  | jq -r '.items[]
-    | select(
-        any(.status.conditions[]?; (.type == "Complete" or .type == "Failed") and .status == "True")
-      )
-    | .metadata.name' \
-  | xargs -r kubectl delete job -n default
