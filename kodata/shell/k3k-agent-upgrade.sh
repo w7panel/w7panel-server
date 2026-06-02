0958917456f583b93ca7ecc56e083bdd03f5f14a
fykyx521 <446897682@qq.com>
2026-06-02 02:56:30 +0000

    fix: upgrade-shell

--- kodata/shell/k3k-agent-upgrade.sh
+++ kodata/shell/k3k-agent-upgrade.sh
@@ -129,10 +129,7 @@ kubectl apply -f $KO_DATA_PATH/crds --server-side
 # w7panel sitemanager-upgrade --version=1.0.26 --identifie=w7_nodejs --is-agent=true
 # w7panel sitemanager-upgrade --version=1.0.26 --identifie=w7_python --is-agent=true
 # w7panel sitemanager-upgrade --version=1.0.25 --identifie=w7_sitemanager --is-agent=true
-<<<<<<< HEAD
 # add k3s.config
-=======
->>>>>>> dev-v1
 
 echo "删除旧的microapp"
 kubectl -n default delete microapp -l microapp.w7.cc/from=root | echo "clear root microapp"
