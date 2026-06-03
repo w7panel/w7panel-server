93c5005a98668d5ffd8133f0318b8a93fd13ff88
fykyx521 <446897682@qq.com>
2026-06-03 08:18:17 +0000

    fix: static proxy

--- app/application/http/controller/static.go
+++ app/application/http/controller/static.go
@@ -50,16 +50,16 @@ func (self Static) StaticInfo(http *gin.Context) {
 			// 拼好 proxy 地址 zpkUrl base64url 编码后放入 path
 			if zpkUrl != "" {
 				zpkUrlEncoded := base64.RawURLEncoding.EncodeToString([]byte(zpkUrl))
-				proxyUrl = fmt.Sprintf("/panel-api/v1/static/%s/%s/%s/frontend/", zpkUrlEncoded, identifie, version)
+				proxyUrl = fmt.Sprintf("/panel-api/v1/static/%s/%s/%s/frontend/index.html", zpkUrlEncoded, identifie, version)
 			}
 		}
 	}
 
 	self.JsonResponseWithoutError(http, gin.H{
-		"status":    status,
-		"proxyUrl":  proxyUrl,
-		"zpkUrl":    zpkUrl,
-		"ticket":    ticket,
+		"status":   status,
+		"proxyUrl": proxyUrl,
+		"zpkUrl":   zpkUrl,
+		"ticket":   ticket,
 	})
 }
 
