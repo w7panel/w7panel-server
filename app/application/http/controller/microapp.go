2c3494394f37b9aa9333d65bc47b436284f80239
fykyx521 <446897682@qq.com>
2026-06-24 18:24:03 +0800

    feat: add microapp front props endpoint

--- app/application/http/controller/microapp.go
+++ app/application/http/controller/microapp.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/w7panel/w7panel/common/service/k8s/microapp"
+	"github.com/w7panel/w7panel/common/service/oidc"
 	microappv1 "github.com/w7panel/w7panel/k8s/pkg/apis/microapp/v1alpha1"
 	"github.com/we7coreteam/w7-rangine-go/v2/src/http/controller"
 )
@@ -36,4 +37,49 @@ func (self MicroApp) Info(http *gin.Context) {
 	}
 	self.JsonResponseWithoutError(http, microapp)
 
+}
+
+func (self MicroApp) FrontProps(http *gin.Context) {
+	token := http.MustGet("k8s_token").(string)
+	name := http.Param("name")
+	item, err := microapp.ListInfo(token, name)
+	if err != nil {
+		self.JsonResponseWithServerError(http, err)
+		return
+	}
+	replace, err := microapp.NewMicroAppReplace(token)
+	if err != nil {
+		self.JsonResponseWithServerError(http, err)
+		return
+	}
+
+	role := replace.GetRole()
+	accessToken := ""
+	server, err := oidc.GetServer()
+	if err == nil && server != nil {
+		accessToken, err = server.CreateDefaultAccessToken(server.ContextWithIssuer(http.Request.Context(), http.Request), replace.Name)
+		if err != nil {
+			accessToken = ""
+		}
+	}
+
+	cloudAccessToken := ""
+	if replace.GetConsoleOpenId() != "" {
+		token, err := microapp.GetCloudAccessToken(replace.GetConsoleOpenId())
+		if err == nil {
+			cloudAccessToken = token
+		}
+	}
+
+	self.JsonResponseWithoutError(http, map[string]string{
+		"url":               item.RoleServerUrl(role),
+		"group":             item.Name,
+		"userid":            replace.Name,
+		"role":              role,
+		"access_token":      accessToken,
+		"openid":            replace.GetConsoleOpenId(),
+		"nickname":          replace.GetNickName(),
+		"cloud_uid":         replace.GetConsoleId(),
+		"cloud_accesstoken": cloudAccessToken,
+	})
 }
