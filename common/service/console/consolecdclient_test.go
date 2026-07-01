6558e35c8ddbae67022160ebb2e54bd7dfe2e796
fykyx521 <446897682@qq.com>
2026-07-01 08:05:30 +0000

    feat: fix

--- common/service/console/consolecdclient_test.go
+++ common/service/console/consolecdclient_test.go
@@ -1,10 +1,8 @@
 package console
 
 import (
-	"os"
 	"testing"
 
-	"github.com/w7panel/w7panel/common/helper"
 	"github.com/w7panel/w7panel/common/service/config"
 	"github.com/w7panel/w7panel/common/service/k8s"
 	// "github.com/w7panel/w7panel/common/service/config"
@@ -26,43 +24,4 @@ func TestRefreshUseCdToken(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-}
-
-func TestVerifyCert(t *testing.T) {
-	cert := `-----BEGIN CERTIFICATE-----
-MIIEKDCCAxCgAwIBAgIBAjANBgkqhkiG9w0BAQsFADCBrDELMAkGA1UEBhMCQ04x
-DTALBgNVBAgMBHRlYW0xDjAMBgNVBAcMBTc2MDUyMQswCQYDVQQKDAJ3NzEdMBsG
-A1UECwwUMjAyNTA2MTIxNDMyMTZMRDFISFgxKzApBgNVBAMMIjIwMjUwNjEyMTQz
-MjE2TEQxSEhYLmxpY2Vuc2UudzcuY2MxJTAjBgkqhkiG9w0BCQEWFmFkbWluQHRl
-c3QuZXhhbXBsZS5jb20wHhcNMjUwNjEyMDYzMjQwWhcNMjUwOTExMDYzMjQwWjCB
-rDELMAkGA1UEBhMCQ04xDTALBgNVBAgMBHRlYW0xDjAMBgNVBAcMBTc2MDUyMQsw
-CQYDVQQKDAJ3NzEdMBsGA1UECwwUMjAyNTA2MTIxNDMyMTZMRDFISFgxKzApBgNV
-BAMMIjIwMjUwNjEyMTQzMjE2TEQxSEhYLmxpY2Vuc2UudzcuY2MxJTAjBgkqhkiG
-9w0BCQEWFmFkbWluQHRlc3QuZXhhbXBsZS5jb20wggEiMA0GCSqGSIb3DQEBAQUA
-A4IBDwAwggEKAoIBAQCeO2nFYwy5tJYkAj/MHhDtfWJyBAIS5DX/HMVBqt4aoRau
-bCpavNyqCIbmkxReTc9BgsAmRumyNs7BI6p+4nUK1FeHPs1b+2DvejzvK2e5nAL8
-dOmUn9L5LbQsMJmPH2qkv7t/0XvEsW/B9rZtqY0Y5rbYxzQfsfHS6SGHCCVCjfUW
-LbENV1unAyG1WOW2DcM2NlFXITdvGNm98L4Xu/dP2li8QdkIBTfDoBOj2za0dFTo
-WzdhDxKKIYmQmmZrTNgh2VSzZrA0JAJ/l4q3zHCk9Sdvy8K9S4XNqf3vjct393sK
-zmUxSMlTisTCVJXQLy9NuDrXjsMZUwsksd03n9ODAgMBAAGjUzBRMB0GA1UdDgQW
-BBRJJOZfxYgoj6J3U8+VZ2ThwJAIgDAfBgNVHSMEGDAWgBRJJOZfxYgoj6J3U8+V
-Z2ThwJAIgDAPBgNVHRMBAf8EBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQA8t7gK
-9zQi9PD459xneSVlseiOIOSXmw2Bt8Qt5jk+QWUkyGk+Sf/4Xc4eSAEjBN73cS4E
-czky8AsslgfU/m5kNIRJYKMJouwO/E6prKF6soBLe/nAgjndW6VfLeDVp38xBh29
-8a0N/bktEYKU8e/ThpsdN6zRZlQlbAfUv6i7iVptUZI1QUIi0cai/M2W3hq6tMgQ
-32USxh3yYlwh3N3HnS4JrFQmYQehW747KWJV7AvRrcfcoUHsQAaaxTgTEEC6mXAf
-o/Cibki8ZP6aYdu3ZSVFWJlTH2Rs2sAg0A3SzT7rAox0PETvLctCA0aIfNIqYnrD
-nNvPNp8iHEnLR5Vq
------END CERTIFICATE-----
-`
-	os.Setenv("USER_AGENT", "we7test-beta")
-	parsedCert, err := helper.ParseX509([]byte(cert))
-	if err != nil {
-		t.Error("xxxx", "err", err)
-	}
-	verify, err := VerifyCert(parsedCert)
-	if err != nil {
-		t.Error("xxxx", "err", err)
-	}
-	print(verify)
 }
