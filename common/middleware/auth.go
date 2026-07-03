9ae7756a2fb5171af794b36df0ba0e45eb70cb30
fykyx521 <446897682@qq.com>
2026-07-03 08:01:47 +0000

    feat: 子集群agent 跳过权限验证

--- common/middleware/auth.go
+++ common/middleware/auth.go
@@ -48,7 +48,7 @@ func (self Auth) Process(ctx *gin.Context) {
 		if saName, err := k8sToken.GetUserName(); err == nil {
 			ctx.Set("username", saName)
 			// TODO 兼容非 k3k 集群，非k3k 集群不校验权限
-			if !k8sToken.IsK3kCluster() && !self.authorizeUserOrServiceAccount(ctx, saName) {
+			if !k8sToken.IsK3kCluster() && !helper.IsChildAgent() && !self.authorizeUserOrServiceAccount(ctx, saName) {
 				return
 			}
 		}
@@ -71,7 +71,7 @@ func (self Auth) Process(ctx *gin.Context) {
 	if err == nil {
 		ctx.Set("username", userName)
 		// TODO 兼容非 k3k 集群，非k3k 集群不校验权限 //TODO 安全隐患
-		if !k8sToken.IsK3kCluster() && !self.authorizeUserOrServiceAccount(ctx, userName) {
+		if !k8sToken.IsK3kCluster() && !helper.IsChildAgent() && !self.authorizeUserOrServiceAccount(ctx, userName) {
 			return
 		}
 	}
