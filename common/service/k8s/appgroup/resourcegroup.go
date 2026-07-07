167b7461377f35945f0b953fe42a0e2c4fd52790
rxw <1120309488@qq.com>
2026-07-07 13:44:42 +0800

    还原 appgroups 的修改

--- common/service/k8s/appgroup/resourcegroup.go
+++ common/service/k8s/appgroup/resourcegroup.go
@@ -1,6 +1,14 @@
 package appgroup
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	"strings"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	visibleGroupsKey = "w7.cc/visible-groups"
+)
 
 func getResourceGroupName(labels map[string]string) string {
 	if labels == nil {
@@ -36,12 +44,19 @@ func getResourceGroupNames(obj metav1.Object) []string {
 	labels := obj.GetLabels()
 	if labels != nil {
 		add(labels["group"], labels["w7.cc/group-name"], labels["w7.cc/release-name"], labels["app.kubernetes.io/instance"], labels["w7.cc/suffix"])
+		add(splitGroupNames(labels[visibleGroupsKey])...)
 	}
 	annotations := obj.GetAnnotations()
 	if annotations != nil {
 		add(annotations["meta.helm.sh/release-name"])
 	}
 	return names
+}
+
+func splitGroupNames(value string) []string {
+	return strings.FieldsFunc(value, func(r rune) bool {
+		return r == ',' || r == ';' || r == ' ' || r == '\t' || r == '\n'
+	})
 }
 
 func containsString(values []string, value string) bool {
@@ -51,18 +66,6 @@ func containsString(values []string, value string) bool {
 		}
 	}
 	return false
-}
-
-func sameStringSet(left, right []string) bool {
-	if len(left) != len(right) {
-		return false
-	}
-	for _, item := range left {
-		if !containsString(right, item) {
-			return false
-		}
-	}
-	return true
 }
 
 func resourceVisibleInGroup(obj metav1.Object, groupName string) bool {
