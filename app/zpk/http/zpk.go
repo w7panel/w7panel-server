3efcebd672b0478397e6b0ae1eb8a7472cff80f7
fykyx521 <446897682@qq.com>
2026-08-11 11:17:52 +0000

    feat: 还原安装限制

--- app/zpk/http/zpk.go
+++ app/zpk/http/zpk.go
@@ -202,10 +202,10 @@ func (self Zpk) Install(http *gin.Context) {
 	token := http.MustGet("k8s_token").(string)
 	k8sToken := k8s.NewK8sToken(token)
 	// 普通用户curl 安装主集群问题
-	// if !k8sToken.IsFounder() && !helper.IsChildAgent() { // ckm 未升级导致判断 有误
-	// 	self.JsonResponseWithServerError(http, errors.New("only founder can install"))
-	// 	return
-	// }
+	if !k8sToken.IsFounder() && !k8sToken.IsK3kCluster() { // ckm 未升级导致判断 有误
+		self.JsonResponseWithServerError(http, errors.New("only founder can install"))
+		return
+	}
 	client, err := k8s.NewK8sClient().Channel(token)
 	if err != nil {
 		self.JsonResponseWithServerError(http, err)
