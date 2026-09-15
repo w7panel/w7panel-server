3295e13f30fe040a1cc5b3064c374e111de5e660
fykyx521 <446897682@qq.com>
2026-09-15 10:58:24 +0800

    support panel auth in ckm listing

--- app/k3k/http/controller/ckm.go
+++ app/k3k/http/controller/ckm.go
@@ -5,6 +5,8 @@ import (
 	"github.com/w7panel/w7panel/common/service/k8s"
 	v1alpha1 "github.com/w7panel/w7panel/common/service/k8s/ckm/api/v1alpha1"
 	"github.com/w7panel/w7panel/common/service/k8s/user/k3k"
+	k3ktypes "github.com/w7panel/w7panel/common/service/k8s/user/k3k/types"
+	userservice "github.com/w7panel/w7panel/common/service/user"
 	"github.com/we7coreteam/w7-rangine-go/v2/src/http/controller"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -24,13 +26,24 @@ func (self Ckm) Info(http *gin.Context) {
 func (self Ckm) List(http *gin.Context) {
 	token := http.MustGet("k8s_token").(string)
 	namespace := http.Query("namespace")
-	user, err := k3k.TokenToK3kUser(token)
-	if err != nil {
-		self.JsonResponseWithServerError(http, err)
-		return
+	var user *k3ktypes.K3kUser
+	if username := http.GetString("username"); username != "" {
+		u, err := userservice.Get(http.Request.Context(), k8s.NewK8sClient().Sdk, username)
+		if err != nil {
+			self.JsonResponseWithServerError(http, err)
+			return
+		}
+		user = k3ktypes.NewK3kUser(u.ToTyped())
+	} else {
+		var err error
+		user, err = k3k.TokenToK3kUser(token)
+		if err != nil {
+			self.JsonResponseWithServerError(http, err)
+			return
+		}
 	}
 	if !user.IsFounder() {
-		namespace = k8s.NewK8sToken(token).GetNamespace()
+		namespace = user.GetK3kNamespace()
 	}
 	sigClient, err := k8s.NewK8sClient().ToSigClient()
 	if err != nil {
